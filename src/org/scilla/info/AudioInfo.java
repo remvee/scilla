208a12e1844645ebf953eb2a2f92aac6d5d76a87
Remco van 't Veer <rwvtveer@xs4all.nl>
2003-02-02 21:05:22 +0000

    introduced VorbisInfo

--- src/org/scilla/info/AudioInfo.java
+++ src/org/scilla/info/AudioInfo.java
@@ -24,39 +24,43 @@ package org.scilla.info;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.scilla.util.MimeType;
 import org.scilla.util.mp3.*;
+import org.scilla.util.vorbis.*;
 
 /**
  * Audio info.
  *
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  * @author R.W. van 't Veer
  */
 public class AudioInfo extends Info {
     public final static String BITRATE = "bitrate";
     public final static String SAMPLERATE = "samplerate";
     public final static String CHANNELS = "channels";
-    public final static String TITLE = "title";
-    public final static String GENRE = "genre";
-    public final static String COMPOSER = "composer";
-    public final static String PERFORMER = "performer";
-    public final static String CONDUCTOR = "conductor";
-    public final static String RECORDING_DATE = "recdate";
-    public final static String RECORDING_YEAR = "recdate";
-    public final static String RECORDING_LOCATION = "reclocation";
-    public final static String ALBUM = "album";
-    public final static String COMMENT = "comment";
 
-    public final static String CODEC_OGG = "OGG Vorbis Audio";
+    public final static String ALBUM = "album";
+    public final static String ARTIST = "artist";
+    public final static String COMMENT = "comment";
+    public final static String COMPOSER = "composer";
+    public final static String CONDUCTOR = "conductor";
+    public final static String GENRE = "genre";
+    public final static String PERFORMER = "performer";
+    public final static String RECORDING_DATE = "recdate";
+    public final static String RECORDING_LOCATION = "reclocation";
+    public final static String TITLE = "title";
+    public final static String TRKNUM = "trknum";
+
+    public final static String CODEC_VORBIS = "OGG Vorbis Audio";
 
     public AudioInfo (String fname) {
 	String type = MimeType.getTypeFromFilename(fname);
 	if (type.endsWith("/mpeg")) {
 	    setupMPEG(fname);
 	} else if (type.endsWith("/ogg-vorbis")) {
-	    setupOGG(fname);
+	    setupVORBIS(fname);
 	}
     }
 
@@ -118,7 +122,7 @@ public class AudioInfo extends Info {
 
 	    t = v1.getArtist();
 	    if (t != null && t.trim().length() != 0) {
-		setString(PERFORMER, t);
+		setString(ARTIST, t);
 	    }
 
 	    t = v1.getAlbum();
@@ -133,7 +137,7 @@ public class AudioInfo extends Info {
 
 	    t = v1.getYear();
 	    if (t != null && t.trim().length() != 0) {
-		setString(RECORDING_YEAR, t);
+		setString(RECORDING_DATE, t);
 	    }
 
 	    t = v1.getTitle();
@@ -145,6 +149,8 @@ public class AudioInfo extends Info {
 	    if (t != null && t.trim().length() != 0) {
 		setString(GENRE, t);
 	    }
+
+	    setInt(TRKNUM, v1.getTrkNum());
 	} catch (Throwable ex) {
 	    // ignore
 	}
@@ -158,104 +164,60 @@ public class AudioInfo extends Info {
 	}
     }
 
-    private void setupOGG (String fname) {
-	// from /etc/magic:
-	//   # vorbis:  file(1) magic for Ogg/Vorbis files
-	//   #
-	//   # From Felix von Leitner <leitner@fefe.de>
-	//   # Extended by Beni Cherniavsky <cben@crosswinds.net>
-	//   #
-	//   # Most (everything but the number of channels and bitrate) is commented
-	//   # out with `##' as it's not interesting to the average user.  The most
-	//   # probable things advanced users would want to uncomment are probably
-	//   # the number of comments and the encoder version.
-	//   #
-	//   # --- Ogg Framing ---
-	//   0		string		OggS		Ogg data
-	//   >4		byte		!0		UNKNOWN REVISION %u
-	//   ##>4		byte		0		revision 0
-	//   >4		byte		0
-	//   ##>>14		lelong		x		(Serial %lX)
-	//   # --- First vorbis packet - general header ---
-	//   >>28		string		\x01vorbis	\b, Vorbis audio,
-	//   >>>35		lelong		!0		UNKNOWN VERSION %lu,
-	//   ##>>>35		lelong		0		version 0,
-	//   >>>35		lelong		0
-	//   >>>>39		ubyte		1		mono,
-	//   >>>>39		ubyte		2		stereo,
-	//   >>>>39		ubyte		>2		%u channels,
-	//   >>>>40		lelong		x		%lu Hz
-	//   # Minimal, nominal and maximal bitrates specified when encoding
-	//   >>>>48		string		<\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff	\b,
-	//   # The above tests if at least one of these is specified:
-	//   >>>>>44		lelong		!-1
-	//   >>>>>>44	lelong		x		>%lu
-	//   >>>>>48		lelong		!-1
-	//   >>>>>>48	lelong		x		~%lu
-	//   >>>>>52		lelong		!-1
-	//   >>>>>>52	lelong		x		<%lu
-	//   >>>>>48		string		<\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff	kbps
-	//   # -- Second vorbis header packet - the comments
-	//   >>>102		string		\x03vorbis
-	//   # A kludge to read the vendor string.  It's a counted string, not a
-	//   # zero-terminated one, so file(1) can't read it in a generic way.
-	//   # libVorbis is the only one existing currently, so I detect specifically
-	//   # it.  The interesting value is the cvs date (8 digits decimal).
-	//   ##>>>>113		string/c	Xiphophorus\ libVorbis\ I	\b, created by: Xiphophorus libVorbis I
-	//   ##>>>>>137	string		>00000000	%.8s
-	//   # Map to beta version numbers:
-	//   ##>>>>>>137	string		<20000508	(<beta1 - prepublic)
-	//   # The string has not changed from beta1 to 2 - they are indistinguishable.
-	//   ##>>>>>>137	string		20000508	(beta1/2)
-	//   ##>>>>>>137	string		>20000508
-	//   ##>>>>>>>137	string		<20001031	(beta2-3)
-	//   ##>>>>>>137	string		20001031	(beta3)
-	//   ##>>>>>>137	string		>20001031
-	//   ##>>>>>>>137	string		<20010225	(beta3-4)
-	//   ##>>>>>>137	string		20010225	(beta4)
-	//   ##>>>>>>137	string		>20010225
-	//   ##>>>>>>>137	string		<20010615	(beta4-RC1)
-	//   ##>>>>>>137	string		20010615	(RC1)
-	//   ##>>>>>>137	string		>20010615	(>RC1)
-	//   # Then come the comments, again length-counted (and number-counted).
-	//   # Some looping constructs and registers would allow reading them but now
-	//   # it's impossible.  However we can print the number of comments present
-	//   # (skipping by the vendor string length):
-	//   ##>>>>(109.l.113)	lelong		0		\b, no comments
-	//   ##>>>>(109.l+113)	lelong		>0		\b, %lu comments
-	FileInputStream in = null;
+    private void setupVORBIS (String fname) {
+	// read vorbis info
+	InputStream in = null;
 	try {
 	    in = new FileInputStream(fname);
-	    byte[] d = new byte[54];
-	    if (in.read(d) == 54
-		    && d[0] == (byte) 'O' && d[1] == (byte) 'g'
-		    && d[2] == (byte) 'g' && d[3] == (byte) 'S'
-		    && d[28] == (byte) 0x01 && d[29] == (byte) 'v'
-		    && d[30] == (byte) 'o' && d[31] == (byte) 'r'
-		    && d[32] == (byte) 'b' && d[33] == (byte) 'i'
-		    && d[34] == (byte) 's') {
-		setString(CODEC, CODEC_OGG);
-		setInt(CHANNELS, (d[39] & 0xff));
-		setInt(SAMPLERATE, ((d[43] & 0xff) << 24)
-			+ ((d[42] & 0xff) << 16)
-			+ ((d[41] & 0xff) << 8)
-			+ (d[40] & 0xff));
-		setInt(BITRATE, ((d[51] & 0xff) << 24)
-			+ ((d[50] & 0xff) << 16)
-			+ ((d[49] & 0xff) << 8)
-			+ (d[48] & 0xff));
-	    } else {
-		setString(CODEC, "corrupted OGG file");
+	    VorbisInfo info = new VorbisInfo(in);
+	    setString(CODEC, CODEC_VORBIS);
+	    setInt(BITRATE, info.getBitrate());
+	    setInt(SAMPLERATE, info.getSampleRate());
+	    setInt(CHANNELS, info.getChannels());
+
+	    String t;
+	    t = info.getComment("ALBUM");
+	    if (t != null && t.trim().length() != 0) {
+		setString(ALBUM, t);
+	    }
+	    t = info.getComment("ARTIST");
+	    if (t != null && t.trim().length() != 0) {
+		setString(ARTIST, t);
+	    }
+	    t = info.getComment("DESCRIPTION");
+	    if (t != null && t.trim().length() != 0) {
+		setString(COMMENT, t);
+	    }
+	    t = info.getComment("GENRE");
+	    if (t != null && t.trim().length() != 0) {
+		setString(GENRE, t);
+	    }
+	    t = info.getComment("PERFORMER");
+	    if (t != null && t.trim().length() != 0) {
+		setString(PERFORMER, t);
+	    }
+	    t = info.getComment("DATE");
+	    if (t != null && t.trim().length() != 0) {
+		setString(RECORDING_DATE, t);
+	    }
+	    t = info.getComment("LOCATION");
+	    if (t != null && t.trim().length() != 0) {
+		setString(RECORDING_LOCATION, t);
+	    }
+	    t = info.getComment("TITLE");
+	    if (t != null && t.trim().length() != 0) {
+		setString(TITLE, t);
+	    }
+	    try {
+		setInt(TRKNUM, Integer.parseInt(info.getComment("TRACKNUMBER")));
+	    } catch (Throwable ex) {
+		// ignore
 	    }
 	} catch (Throwable ex) {
-	    // ignore
+	    // ignore..
 	} finally {
 	    if (in != null) {
-		try {
-		    in.close();
-		} catch (IOException ex) {
-		    // ignore
-		}
+		try { in.close(); } catch (IOException ex) { /* ignore */ }
 	    }
 	}
     }
---
+++ src/org/scilla/util/vorbis/VorbisInfo.java
@@ -0,0 +1,241 @@
+/*
+ * scilla
+ *
+ * Copyright (C) 2003  R.W. van 't Veer
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+package org.scilla.util.vorbis;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Remco van 't Veer
+ * @version $Revision: 1.1 $
+ */
+public class VorbisInfo {
+    public final String PAGE_MARKER = "OggS";
+    public final String VORBIS_MARKER = "vorbis";
+
+    private int vorbisVersion;
+    private int channels;
+    private int sampleRate;
+    private int maxBitrate;
+    private int bitrate;
+    private int minBitrate;
+    private String vendor;
+    private Map commentMap;
+
+    public VorbisInfo (InputStream in)
+    throws Exception {
+	// first 2 frames should be enough..
+	Frame f1 = new Frame(in);
+	Frame f2 = new Frame(in);
+	// append page data
+	byte[] data = new byte[f1.length + f2.length];
+	System.arraycopy(f1.data, 0, data, 0, f1.length);
+	System.arraycopy(f2.data, 0, data, f1.length, f2.length);
+	// identification header
+	{
+	    if (data[0] != 1 || ! hasVorbisMarker(data, 1)) {
+		throw new Exception("didn't find vorbis identification header");
+	    }
+	    int pos = 7;
+	    vorbisVersion = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+		    + ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+	    channels = (int) data[pos++];
+	    sampleRate = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+		    + ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+	    maxBitrate = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+		    + ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+	    bitrate = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+		    + ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+	    minBitrate = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+		    + ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+	}
+	// comment header
+	{
+	    if (data[30] != 3 || ! hasVorbisMarker(data, 31)) {
+		throw new Exception("didn't find vorbis comment header");
+	    }
+	    int pos = 37;
+
+	    {
+		int l = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+			+ ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < l; i++) {
+		    sb.append((char) data[pos++]);
+		}
+		vendor = sb.toString();
+	    }
+
+	    commentMap = new HashMap();
+	    int commentLen = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+		    + ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+	    for (int i = 0; i < commentLen; i++) {
+		int l = (data[pos++] & 0xff) + ((data[pos++] & 0xff) << 8)
+			+ ((data[pos++] & 0xff) << 16) + ((data[pos++] & 0xff) << 24);
+		StringBuffer sb = new StringBuffer();
+		for (int j = 0; j < l; j++) {
+		    sb.append((char) data[pos++]);
+		}
+		String v = sb.toString();
+		String key = v.substring(0, v.indexOf('='));
+		String val = v.substring(v.indexOf('=') + 1);
+		commentMap.put(key, val);
+	    }
+	}
+    }
+
+    public int getVorbisVersion () {
+	return vorbisVersion;
+    }
+    public int getChannels () {
+	return channels;
+    }
+    public int getSampleRate () {
+	return sampleRate;
+    }
+    public int getMaximumBitrate () {
+	return maxBitrate;
+    }
+    public int getBitrate () {
+	return bitrate;
+    }
+    public int getMinimumBitrate () {
+	return minBitrate;
+    }
+    public String getVendor () {
+	return vendor;
+    }
+    public String getComment (String key) {
+	return (String) commentMap.get(key);
+    }
+    public Set getCommentKeys () {
+	return new HashSet(commentMap.keySet());
+    }
+
+    public String toString () {
+	return
+	    "vorbisVersion="+vorbisVersion+
+	    "\nchannels="+channels+
+	    "\nsampleRate="+sampleRate+
+	    "\nmaxBitrate="+maxBitrate+
+	    "\nnominalBitrate="+bitrate+
+	    "\nminBitrate="+minBitrate+
+	    "\nvendor="+vendor+
+	    "\ncomments="+commentMap;
+    }
+
+    private boolean hasVorbisMarker (byte[] data, int offset) {
+	char[] marker = VORBIS_MARKER.toCharArray();
+	for (int i = 0; i < marker.length; i++) {
+	    if (data[offset + i] != marker[i]) {
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    /** debug */
+    public static void main (String[] args)
+    throws Exception {
+	for (int i = 0; i < args.length; i++) {
+	    System.out.println(args[i]);
+	    FileInputStream in = new FileInputStream(args[i]);
+	    VorbisInfo info = new VorbisInfo(in);
+	    in.close();
+	    System.out.println("  "+info);
+	}
+    }
+
+    private class Frame {
+	int revision;
+	int headerType;
+	long granulePosition;
+	int serialNumber;
+	int pageSequence;
+	int pageChecksum;
+	int pageSegments;
+	int[] segmentTable;
+	int length;
+	byte[] data;
+
+	Frame (InputStream in)
+	throws IOException {
+	    // read page marker
+	    char[] marker = PAGE_MARKER.toCharArray();
+	    for (int i = 0; i < marker.length; i++) {
+		if ((char)in.read() != marker[i]) {
+		    throw new IOException("NOT AN OGG VORBIS FILE");
+		}
+	    }
+	    // read page parameters
+	    revision = in.read();
+	    headerType = in.read();
+	    granulePosition = readLong(in);
+	    serialNumber = readInt(in);
+	    pageSequence = readInt(in);
+	    pageChecksum = readInt(in);
+	    pageSegments = in.read();
+
+	    // read segment table and determine data length
+	    segmentTable = new int[pageSegments];
+	    length = 0;
+	    for (int i = 0; i < pageSegments; i++) {
+		segmentTable[i] = in.read();
+		length += segmentTable[i];
+	    }
+
+	    // read data
+	    data = new byte[length];
+	    if (in.read(data) != length) {
+		throw new IOException("NOT AN OGG VORBIS FILE");
+	    }
+	}
+
+	private int readInt (InputStream in)
+	throws IOException {
+	    int c0 = in.read();
+	    int c1 = in.read();
+	    int c2 = in.read();
+	    int c3 = in.read();
+	    return c0 + (c1 << 8) + (c1 << 16) + (c1 << 24);
+	}
+
+	private long readLong (InputStream in)
+	throws IOException {
+	    long c0 = (long) in.read();
+	    long c1 = (long) in.read();
+	    long c2 = (long) in.read();
+	    long c3 = (long) in.read();
+	    long c4 = (long) in.read();
+	    long c5 = (long) in.read();
+	    long c6 = (long) in.read();
+	    long c7 = (long) in.read();
+	    return c0 + (c1 << 8) + (c1 << 16) + (c1 << 24) +
+		    (c1 << 32) + (c1 << 40) + (c1 << 48) + (c1 << 56);
+	}
+    }
+}
