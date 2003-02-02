e99e6a311c9cd6297f3af71fb70e6fb477e89dd7
Remco van 't Veer <rwvtveer@xs4all.nl>
2003-02-02 14:59:22 +0000

    simple ogg vorbis magic

--- src/org/scilla/info/AudioInfo.java
+++ src/org/scilla/info/AudioInfo.java
@@ -22,6 +22,8 @@
 package org.scilla.info;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import org.scilla.util.MimeType;
 import org.scilla.util.mp3.*;
@@ -29,13 +31,13 @@ import org.scilla.util.mp3.*;
 /**
  * Audio info.
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  * @author R.W. van 't Veer
  */
 public class AudioInfo extends Info {
     public final static String BITRATE = "bitrate";
+    public final static String SAMPLERATE = "samplerate";
     public final static String CHANNELS = "channels";
-
     public final static String TITLE = "title";
     public final static String GENRE = "genre";
     public final static String COMPOSER = "composer";
@@ -47,6 +49,8 @@ public class AudioInfo extends Info {
     public final static String ALBUM = "album";
     public final static String COMMENT = "comment";
 
+    public final static String CODEC_OGG = "OGG Vorbis Audio";
+
     public AudioInfo (String fname) {
 	String type = MimeType.getTypeFromFilename(fname);
 	if (type.endsWith("/mpeg")) {
@@ -56,6 +60,20 @@ public class AudioInfo extends Info {
 	}
     }
 
+    /**
+     * @return Kbits per second or <tt>-1</tt> when unknown.
+     */
+    public int getBitRate () {
+	return getInt(BITRATE);
+    }
+
+    /**
+     * @return number of channels or <tt>-1</tt> when unknown.
+     */
+    public int getChannels () {
+	return getInt(CHANNELS);
+    }
+
     private void setupMPEG (String fname) {
 	File f = new File(fname);
 
@@ -63,7 +81,8 @@ public class AudioInfo extends Info {
 	XingInfo xi = null;
 	try {
 	    xi = new XingInfo(f);
-	    setInt(BITRATE, xi.getBitRate());
+	    setInt(BITRATE, xi.getBitRate() * 1000);
+	    setInt(SAMPLERATE, xi.getSampleRate());
 	    setInt(CHANNELS, xi.isMono() ? 1 : 2);
 	    setString(CODEC, xi.toString());
 	} catch (Throwable ex) {
@@ -75,11 +94,12 @@ public class AudioInfo extends Info {
 	}
 
 	// read frame headers if no xing tag
-	if (xi != null) {
+	if (xi == null) {
 	    FrameHeader fh = null;
 	    try {
 		fh = new FrameHeader(f);
-		setInt(BITRATE, fh.getBitRate());
+		setInt(BITRATE, fh.getBitRate() * 1000);
+		setInt(SAMPLERATE, fh.getSampleRate());
 		setInt(CHANNELS, fh.isMono() ? 1 : 2);
 		setString(CODEC, fh.toString());
 	    } catch (Throwable ex) {
@@ -139,9 +159,104 @@ public class AudioInfo extends Info {
     }
 
     private void setupOGG (String fname) {
-	// determine biterate etc.
-	// TODO
-	// read clip info
-	// TODO
+	// from /etc/magic:
+	//   # vorbis:  file(1) magic for Ogg/Vorbis files
+	//   #
+	//   # From Felix von Leitner <leitner@fefe.de>
+	//   # Extended by Beni Cherniavsky <cben@crosswinds.net>
+	//   #
+	//   # Most (everything but the number of channels and bitrate) is commented
+	//   # out with `##' as it's not interesting to the average user.  The most
+	//   # probable things advanced users would want to uncomment are probably
+	//   # the number of comments and the encoder version.
+	//   #
+	//   # --- Ogg Framing ---
+	//   0		string		OggS		Ogg data
+	//   >4		byte		!0		UNKNOWN REVISION %u
+	//   ##>4		byte		0		revision 0
+	//   >4		byte		0
+	//   ##>>14		lelong		x		(Serial %lX)
+	//   # --- First vorbis packet - general header ---
+	//   >>28		string		\x01vorbis	\b, Vorbis audio,
+	//   >>>35		lelong		!0		UNKNOWN VERSION %lu,
+	//   ##>>>35		lelong		0		version 0,
+	//   >>>35		lelong		0
+	//   >>>>39		ubyte		1		mono,
+	//   >>>>39		ubyte		2		stereo,
+	//   >>>>39		ubyte		>2		%u channels,
+	//   >>>>40		lelong		x		%lu Hz
+	//   # Minimal, nominal and maximal bitrates specified when encoding
+	//   >>>>48		string		<\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff	\b,
+	//   # The above tests if at least one of these is specified:
+	//   >>>>>44		lelong		!-1
+	//   >>>>>>44	lelong		x		>%lu
+	//   >>>>>48		lelong		!-1
+	//   >>>>>>48	lelong		x		~%lu
+	//   >>>>>52		lelong		!-1
+	//   >>>>>>52	lelong		x		<%lu
+	//   >>>>>48		string		<\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff	kbps
+	//   # -- Second vorbis header packet - the comments
+	//   >>>102		string		\x03vorbis
+	//   # A kludge to read the vendor string.  It's a counted string, not a
+	//   # zero-terminated one, so file(1) can't read it in a generic way.
+	//   # libVorbis is the only one existing currently, so I detect specifically
+	//   # it.  The interesting value is the cvs date (8 digits decimal).
+	//   ##>>>>113		string/c	Xiphophorus\ libVorbis\ I	\b, created by: Xiphophorus libVorbis I
+	//   ##>>>>>137	string		>00000000	%.8s
+	//   # Map to beta version numbers:
+	//   ##>>>>>>137	string		<20000508	(<beta1 - prepublic)
+	//   # The string has not changed from beta1 to 2 - they are indistinguishable.
+	//   ##>>>>>>137	string		20000508	(beta1/2)
+	//   ##>>>>>>137	string		>20000508
+	//   ##>>>>>>>137	string		<20001031	(beta2-3)
+	//   ##>>>>>>137	string		20001031	(beta3)
+	//   ##>>>>>>137	string		>20001031
+	//   ##>>>>>>>137	string		<20010225	(beta3-4)
+	//   ##>>>>>>137	string		20010225	(beta4)
+	//   ##>>>>>>137	string		>20010225
+	//   ##>>>>>>>137	string		<20010615	(beta4-RC1)
+	//   ##>>>>>>137	string		20010615	(RC1)
+	//   ##>>>>>>137	string		>20010615	(>RC1)
+	//   # Then come the comments, again length-counted (and number-counted).
+	//   # Some looping constructs and registers would allow reading them but now
+	//   # it's impossible.  However we can print the number of comments present
+	//   # (skipping by the vendor string length):
+	//   ##>>>>(109.l.113)	lelong		0		\b, no comments
+	//   ##>>>>(109.l+113)	lelong		>0		\b, %lu comments
+	FileInputStream in = null;
+	try {
+	    in = new FileInputStream(fname);
+	    byte[] d = new byte[54];
+	    if (in.read(d) == 54
+		    && d[0] == (byte) 'O' && d[1] == (byte) 'g'
+		    && d[2] == (byte) 'g' && d[3] == (byte) 'S'
+		    && d[28] == (byte) 0x01 && d[29] == (byte) 'v'
+		    && d[30] == (byte) 'o' && d[31] == (byte) 'r'
+		    && d[32] == (byte) 'b' && d[33] == (byte) 'i'
+		    && d[34] == (byte) 's') {
+		setString(CODEC, CODEC_OGG);
+		setInt(CHANNELS, (d[39] & 0xff));
+		setInt(SAMPLERATE, ((d[43] & 0xff) << 24)
+			+ ((d[42] & 0xff) << 16)
+			+ ((d[41] & 0xff) << 8)
+			+ (d[40] & 0xff));
+		setInt(BITRATE, ((d[51] & 0xff) << 24)
+			+ ((d[50] & 0xff) << 16)
+			+ ((d[49] & 0xff) << 8)
+			+ (d[48] & 0xff));
+	    } else {
+		setString(CODEC, "corrupted OGG file");
+	    }
+	} catch (Throwable ex) {
+	    // ignore
+	} finally {
+	    if (in != null) {
+		try {
+		    in.close();
+		} catch (IOException ex) {
+		    // ignore
+		}
+	    }
+	}
     }
 }
