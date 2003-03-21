abfe2fdde1806b19d5fb6c0274ddbbcae014f031
Remco van 't Veer <rwvtveer@xs4all.nl>
2003-03-21 12:45:48 +0000

    added listtype and streamtype properties to playlist tag

--- examples/src/test/PlaylistTag.java
+++ examples/src/test/PlaylistTag.java
@@ -10,35 +10,83 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+/**
+ * The playlist tag puts an url to a playlist servlet into an JSP
+ * attributes.
+ *
+ * @author R.W. van 't Veer
+ * @version $Revision: 1.5 $
+ */
 public class PlaylistTag extends TagSupport {
     /** logger */
     private static final Log log = LogFactory.getLog(PlaylistTag.class);
 
-    public PlaylistTag () {
-	super();
-    }
+    /** default playlist type */
+    public final static String LIST_TYPE_DEFAULT = "m3u";
+    /** JSP attribute (any scope) to try and get playlist type from */
+    public final static String LIST_TYPE_ATTRIBUTE = "playlistListType";
+    /** default stream type */
+    public final static String STREAM_TYPE_DEFAULT = "ogg";
+    /** JSP attribute (any scope) to try and get stream type from */
+    public final static String STREAM_TYPE_ATTRIBUTE = "playlistStreamType";
+    /** basename of the playlist servlet; without ".m3u" or ".pls" */
+    public final static String PLAYLIST_SERVLET_BASE = "playlist";
 
     public int doStartTag ()
     throws JspException {
-	// build playlist url
-	String url = "playlist.m3u?d=";
+	StringBuffer out = new StringBuffer();
+
+	// select servlet
+	out.append(PLAYLIST_SERVLET_BASE);
+	out.append('.');
+	if (listtype != null) {
+	    out.append(listtype);
+	} else {
+	    String t = (String) pageContext.findAttribute(LIST_TYPE_ATTRIBUTE);
+	    if (t != null) {
+		out.append(t);
+	    } else {
+		out.append(LIST_TYPE_DEFAULT);
+	    }
+	}
 
 	// handle location
+	out.append('?');
+	out.append(PlaylistServlet.PATH_PARAM);
+	out.append('=');
 	Object obj = pageContext.findAttribute(name);
 	if (obj instanceof DirectoryBean) {
-	    url += URLEncoder.encode(((DirectoryBean)obj).getPath());
+	    out.append(URLEncoder.encode(((DirectoryBean)obj).getPath()));
 	} else if (obj instanceof TrackBean) {
-	    url += URLEncoder.encode(((TrackBean)obj).getFilename());
+	    out.append(URLEncoder.encode(((TrackBean)obj).getFilename()));
 	} else {
 	    throw new JspException("oeps, unknown stream");
 	}
 
+	// stream type
+	out.append('&');
+	out.append(PlaylistServlet.OUTPUT_TYPE_PARAM);
+	out.append('=');
+	if (streamtype != null) {
+	    out.append(streamtype);
+	} else {
+	    String t = (String) pageContext.findAttribute(STREAM_TYPE_ATTRIBUTE);
+	    if (t != null) {
+		out.append(t);
+	    } else {
+		out.append(STREAM_TYPE_DEFAULT);
+	    }
+	}
+
 	// handle recursion
 	if (recursive) {
-	    url += "&r=1";
+	    out.append('&');
+	    out.append(PlaylistServlet.RECURS_PARAM);
+	    out.append("=1");
 	}
 
 	// encode session
+	String url = out.toString();
 	url = ((HttpServletResponse)pageContext.getResponse()).encodeURL(url);
 
 	// place var attribute in page context
@@ -47,40 +95,60 @@ public class PlaylistTag extends TagSupport {
         return SKIP_BODY;
     }
 
+    /**
+     * @param v name of attribute containing directory or track bean
+     */
     public void setName (String v) {
 	name = v;
     }
-    public String getName () {
-	return name;
-    }
     private String name;
 
+    /**
+     * @param v name of attribute to put url in
+     */
     public void setVar (String v) {
 	var = v;
     }
-    public String getVar () {
-	return var;
-    }
     private String var;
 
+    /**
+     * @param v recursive option, set to <tt>true</tt> of recursion
+     */
     public void setRecursive (String v) {
 	recursive = (new Boolean(v)).booleanValue();
     }
-    public String getRecursive () {
-	return (new Boolean(recursive)).toString();
+    private boolean recursive = false;
+
+    /**
+     * @param v type of playlist, i.e. <tt>m3u</tt> or <tt>pls</tt>
+     */
+    public void setListtype (String v) {
+	listtype = v;
     }
-    private boolean recursive = false;;
+    private String listtype = null;
 
+    /**
+     * @param v type of stream, i.e. <tt>ogg</tt> or <tt>mp3</tt>
+     */
+    public void setStreamtype (String v) {
+	streamtype = v;
+    }
+    private String streamtype = STREAM_TYPE_DEFAULT;
 
+    /**
+     * @param v JSP scope to put <tt>var</tt> attribute in
+     */
     public void setScope (String v) {
 	scope = v;
     }
-    public String getScope () {
-	return scope;
-    }
     private String scope;
 
-    public int getScopeInt ()
+    /**
+     * @return JSP scope as an integer
+     * @see javax.servlet.jsp.PageContext
+     * @throws JspException when scope not a valid value
+     */
+    private int getScopeInt ()
     throws JspException {
 	if (scope == null || "page".equalsIgnoreCase(scope)) {
 	    return PageContext.PAGE_SCOPE;
