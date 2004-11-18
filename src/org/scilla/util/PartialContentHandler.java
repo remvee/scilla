2d5d2e21fdde5e3a26087ad52c04aec63e02cbcb
Remco van 't Veer <rwvtveer@xs4all.nl>
2004-11-18 14:33:02 +0000

    reindent

--- src/org/scilla/util/PartialContentHandler.java
+++ src/org/scilla/util/PartialContentHandler.java
@@ -35,7 +35,7 @@ import org.apache.commons.logging.LogFactory;
 /**
  * Handle (partial) content requests.
  *
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  * @author R.W. van 't Veer
  */
 public class PartialContentHandler {
@@ -62,91 +62,93 @@ public class PartialContentHandler {
      * in which case partial content request are handled as normal requests
      * @throws IOException when reading or writing fails
      */
-    public static void process(HttpServletRequest request, HttpServletResponse response, InputStream in, long len)
-    throws IOException {
-		OutputStream out = response.getOutputStream();
+    public static void process(HttpServletRequest request,
+            HttpServletResponse response, InputStream in, long len)
+            throws IOException {
+        OutputStream out = response.getOutputStream();
 
-		// can only do partial content when length is unknown
-		if (len != -1) {
-			String rangeHeader = request.getHeader(RANGE_HEADER);
-			// was partial content requested?
-			if (rangeHeader != null && rangeHeader.startsWith(BYTE_RANGE)) {
-				String byteSpec = rangeHeader.substring(BYTE_RANGE.length());
-				int sepPos = byteSpec.indexOf('-');
-				// does the byte spec describe a range?
-				if (sepPos != -1) {
-					long offset = 0;
-					long endpoint = -1;
+        // can only do partial content when length is unknown
+        if (len != -1) {
+            String rangeHeader = request.getHeader(RANGE_HEADER);
+            // was partial content requested?
+            if (rangeHeader != null && rangeHeader.startsWith(BYTE_RANGE)) {
+                String byteSpec = rangeHeader.substring(BYTE_RANGE.length());
+                int sepPos = byteSpec.indexOf('-');
+                // does the byte spec describe a range?
+                if (sepPos != -1) {
+                    long offset = 0;
+                    long endpoint = -1;
 
-					// determine offset
-					if (sepPos > 0) {
-						String s = byteSpec.substring(0, sepPos).trim();
-						offset = Integer.parseInt(s);
-					}
-					// determine endpoint
-					if (sepPos != byteSpec.length() - 1) {
-						String s = byteSpec.substring(sepPos + 1).trim();
-						endpoint = Integer.parseInt(s);
-					} else {
-						endpoint = len - 1;
-					}
-					
-					// notify receiver this is partial content
-					response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-					String contentRange = offset + "-" + endpoint + "/" + len;
-					response.setHeader(CONTENT_RANGE_HEADER, contentRange);
-					log.debug("send range header: " + CONTENT_RANGE_HEADER + ": " + contentRange);
-					response.setContentLength((int) (endpoint - offset + 1));
+                    // determine offset
+                    if (sepPos > 0) {
+                        String s = byteSpec.substring(0, sepPos).trim();
+                        offset = Integer.parseInt(s);
+                    }
+                    // determine endpoint
+                    if (sepPos != byteSpec.length() - 1) {
+                        String s = byteSpec.substring(sepPos + 1).trim();
+                        endpoint = Integer.parseInt(s);
+                    } else {
+                        endpoint = len - 1;
+                    }
 
-					// skip till offset
-					if (offset > 0) {
-						in.skip(offset);
-					}
+                    // notify receiver this is partial content
+                    response.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+                    String contentRange = offset + "-" + endpoint + "/" + len;
+                    response.setHeader(CONTENT_RANGE_HEADER, contentRange);
+                    log.debug("send range header: " + CONTENT_RANGE_HEADER
+                            + ": " + contentRange);
+                    response.setContentLength((int) (endpoint - offset + 1));
 
-					// write partial data
-					int n;
-					byte[] b = new byte[BUFFER_SIZE];
-					while ((n = in.read(b)) != -1) {
-						if (endpoint != -1) {
-							if (offset + n > endpoint) {
-								n = (int) (endpoint - offset) + 1;
-								if (n > 0) {
-									out.write(b, 0, n);
-								}
-								break;
-							}
-							offset += n;
-						}
-						out.write(b, 0, n);
-					}
+                    // skip till offset
+                    if (offset > 0) {
+                        in.skip(offset);
+                    }
 
-					// done
-					return;
-				}
+                    // write partial data
+                    int n;
+                    byte[] b = new byte[BUFFER_SIZE];
+                    while ((n = in.read(b)) != -1) {
+                        if (endpoint != -1) {
+                            if (offset + n > endpoint) {
+                                n = (int) (endpoint - offset) + 1;
+                                if (n > 0) {
+                                    out.write(b, 0, n);
+                                }
+                                break;
+                            }
+                            offset += n;
+                        }
+                        out.write(b, 0, n);
+                    }
 
-				log.info("didn't understand request.. treat as normal request");
-				if (log.isDebugEnabled()) {
-					logHeaders(request);
-				}
-			}
+                    // done
+                    return;
+                }
 
-			// inform client of data size
-			response.setContentLength((int) len);
-		}
+                log.info("didn't understand request.. treat as normal request");
+                if (log.isDebugEnabled()) {
+                    logHeaders(request);
+                }
+            }
 
-		// write all content to client
-		int n;
-		byte[] b = new byte[BUFFER_SIZE];
-		while ((n = in.read(b)) != -1) {
-			out.write(b, 0, n);
-		}
-	}
+            // inform client of data size
+            response.setContentLength((int) len);
+        }
+
+        // write all content to client
+        int n;
+        byte[] b = new byte[BUFFER_SIZE];
+        while ((n = in.read(b)) != -1) {
+            out.write(b, 0, n);
+        }
+    }
 
     private static void logHeaders(HttpServletRequest request) {
-		for (Enumeration en = request.getHeaderNames(); en.hasMoreElements();) {
-			String key = (String) en.nextElement();
-			String val = request.getHeader(key);
-			log.debug("header: " + key + "=" + val);
-		}
-	}
+        for (Enumeration en = request.getHeaderNames(); en.hasMoreElements();) {
+            String key = (String) en.nextElement();
+            String val = request.getHeader(key);
+            log.debug("header: " + key + "=" + val);
+        }
+    }
 }
