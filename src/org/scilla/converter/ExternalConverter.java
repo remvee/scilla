bda021571ea60a8745700629c502775d68c97083
Remco van 't Veer <rwvtveer@xs4all.nl>
2005-04-24 09:04:46 +0000

    remove canExecute implementation because it does not work on most platforms

--- src/org/scilla/converter/ExternalConverter.java
+++ src/org/scilla/converter/ExternalConverter.java
@@ -112,7 +112,7 @@ import org.scilla.*;
  * </DL>
  * @see org.scilla.Config
  * @author R.W. van 't Veer
- * @version $Revision: 1.19 $
+ * @version $Revision: 1.20 $
  */
 public class ExternalConverter implements Converter {
     private static final Log log = LogFactory.getLog(ExternalConverter.class);
@@ -467,16 +467,7 @@ public class ExternalConverter implements Converter {
      * @return <tt>true</tt> if the command can be executed
      */
     private static boolean canExecute(String exec) {
-        String osName = System.getProperty("os.name");
-        if (osName != null && !osName.startsWith("Windows")) {
-            try {
-                // TODO this trick does not work on NT!
-                Runtime.getRuntime().exec(new String[] { exec}).waitFor();
-            } catch (Throwable ex) {
-                log.debug("can't execute: '" + exec + "'", ex);
-                return false;
-            }
-        }
+        // TODO implement me!
         return true;
     }
 }
